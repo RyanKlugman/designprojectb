f11a7597bad216cc2a63f16aff80f24bd4a22674
kevinn20 <38678168+kevinn20@users.noreply.github.com>
2018-05-25 23:26:36 +1000

    test 2
    integration of algorithm with hardware

---
+++ test2/Operations.h
@@ -0,0 +1,40 @@
+//Wavelet operations
+///////////////////////
+
+#ifndef _Operations_h
+#define _Operations_h
+
+#include "Structs.h"
+
+//Downsamples the resolution of the sub-bands (lossy compression)
+void Downsample(unsigned char *input, int &img_size, int steps);
+
+//Rescales everything back to original size
+void Upsample(unsigned char *input, int &img_size, int steps);
+
+//Quantizes the pixel values according to (amount) and scales them so they are all
+//between 0 - 255
+unsigned char *Quantize(float *input, int img_size, int amount, wlt_header_info &wlt);
+
+//Rescales the coefficients to their original values after Quantize(...)
+void Rescale(float *input, int img_size, wlt_header_info wlt);
+
+//Converts from rgb -> yuv colourspace
+void ToYUV(float *input, int img_size);
+
+//Converts from yuv -> rgb colourspace
+void ToRGB(float *input, int img_size);
+
+//These three functions together perform a Daubechies 9/7 transform
+//Transform97() is the controlling function
+void Transform97(float *input, int img_size);
+void TransformStream(float *input, int img_size);
+void Step97(float *input, int img_size);
+
+//These three functions together perform a Daubechies 9/7 inverse transform
+//Inverse97() is the controlling function
+void Inverse97(float *input, int img_size, int steps);
+void InverseStream(float *input, int img_size, int steps);
+void InvStep97(float* input, int img_size);
+
+#endif
---
+++ test2/quantize_hard.cpp
@@ -0,0 +1,113 @@
+#include "Operations.h"
+#include "Structs.h"
+#include "float.cpp"
+#include <string.h>
+#include <math.h>
+
+unsigned char *Quantize_hard(float *input, int img_size, int amount, wlt_header_info &wlt)
+{
+	//if(amount > 64) amount = 64;
+	unsigned char *q = new unsigned char[img_size];
+
+	if(amount > 64) amount = 64;
+	float max=0;
+	int do_scale = 0;
+	for(int i=0; i<img_size; i++)
+	{
+		int abs;
+		if(input[i] < 0) abs = input[i] * (-1);
+		else abs = input[i];
+		if(abs > max) max = abs;
+	}
+	if(max > 255) do_scale = 1;
+	float scale = max / 128;
+	wlt.scale = scale; //we will need this value on decompression
+
+
+	int bram_size = 10;
+	int times = img_size / bram_size;
+	float *partition = new float[bram_size];
+	int reset = 1;
+	int soft_start = 2;
+	//float amount;
+	int int_amount = (int) amount;
+	//float scale;
+	//int doscale;
+	//int done;
+
+	for (int i = 0; i < times; i++) {
+		int k = i * bram_size;
+		int end = (i + 1)*bram_size;
+
+		for (k; k < end; k++) {
+			partition[k - (i*bram_size)] = input[k];
+		}
+		
+		// write to Bram
+		uint32_t a = map();
+		for (int l = 0 ; l < bram_size ; l++){
+			writeToBRAM(l, partition[l],a);
+		}
+		
+		writeToRegister(1, amount, a); //amount
+		a[2] = int_amount;				//int_amount
+		writeToRegister(3, scale, a); //scale
+		a[4] = doscale; //doscale
+		a[0] = start; // assert start
+		// quantise start signal
+		
+	/*	for (int l = 0 ; l < bram_size ; l++){
+			cout << "read at address " + to_string(l) + " : " + to_string(readFromBRAM(l,a)) + "\n";
+		}*/
+		
+			
+		// quantize done signal + read from bram
+		while ((a[5] = 0)){ //polling for done
+			
+		}
+		
+		a[0] = reset;
+		
+		for (int l = 0 ; l < bram_size ; l++){
+			partition[l] = readFromBRAM(l,a);
+		}
+		
+		int k = i * bram_size;
+		int end = (i + 1)*bram_size;
+		for (k; k < end; k++) {
+			input[k] = partition[k - (i*bram_size)];
+		}
+
+
+	}
+	/*int i;
+
+	//first we need to adjust all coefficients to make sure they are between 0 - 255
+	//we will scale them according to the maximum value and then center them around 128
+	float max=0;
+	int do_scale = 0;
+	for(i=0; i<img_size; i++)
+	{
+		int abs;
+		if(input[i] < 0) abs = input[i] * (-1);
+		else abs = input[i];
+		if(abs > max) max = abs;
+	}
+	if(max > 255) do_scale = 1;
+	float scale = max / 128;
+	wlt.scale = scale; //we will need this value on decompression
+
+	for(i=0; i<img_size; i++)
+	{
+		//scale to between 0 - 255
+		if(do_scale == 1) input[i] = (input[i] / scale) + 128;
+		//now, quantize the pixel values
+		input[i] = input[i] / amount;
+		input[i] = (int)(input[i]+0.5); //round up or down
+		input[i] = input[i] * amount; //scale back up
+		if(input[i] > 255) input[i]=255;
+		if(input[i] < 0) input[i]=0;
+		q[i] = ((unsigned char)input[i]);
+	}*/
+	return q;
+}
